d819bb3e20591f4450ad9f2a1ebbb5daf638a25b
Survi <suravitaroy@gmail.com>
2020-07-29 08:45:31 -0400

    removed secret key

---
+++ blogApi/blogs/secrets.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'd%&bm4s9p$7s&@u_jh@@&&=@*)@$$g27$lg_xb0_9swya==^be'
--- blogApi/settings.py
+++ blogApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from blogApi.blogs.secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd%&bm4s9p$7s&@u_jh@@&&=@*)@$$g27$lg_xb0_9swya==^be'
+# SECRET_KEY = 'd%&bm4s9p$7s&@u_jh@@&&=@*)@$$g27$lg_xb0_9swya==^be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
